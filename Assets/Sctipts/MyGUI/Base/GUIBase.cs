434f33717b957922f15a6a4617dc74c05fd5e01b
BaiRong-NUC <gby@edu.nuc.email>
2025-10-14 21:21:12 +0800

    导入MyGUI封装包

---
+++ Assets/Sctipts/MyGUI/Base/GUIBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GUIType
+{
+    On,
+    Off,
+}
+
+
+// 控件共同行为父类
+public abstract class GUIBase : MonoBehaviour
+{
+    // 1. 位置信息
+    public GUIPosition guiPos = new GUIPosition();
+    // 2. 内容信息
+    public GUIContent guiContent = new GUIContent();
+    // 3. 自定义样式
+    public GUIStyle guiStyle = new GUIStyle();
+    // 4. 自定义样式是否开始
+    public GUIType useCustomStyle = GUIType.Off;
+
+    // 给外部提供的绘制GUI方法
+    public void DrawGUI()
+    {
+        if (this.useCustomStyle == GUIType.On)
+        {
+            this.DrawControlUseStyle();
+        }
+        else
+        {
+            this.DrawControlNoStyle();
+        }
+    }
+
+    // 使用自定义风格画控件
+    protected abstract void DrawControlUseStyle();
+
+    // 使用默认风格画控件
+    protected abstract void DrawControlNoStyle();
+}
