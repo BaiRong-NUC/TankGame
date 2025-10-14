9d57bcd608965c5050dbfcc72dc7a8602c6d1b61
BaiRong-NUC <gby@edu.nuc.email>
2025-10-14 22:17:02 +0800

    完成了开始界面的布置

---
+++ Assets/Sctipts/Game/RotateObj.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObj : MonoBehaviour
+{
+    // 使物体旋转
+    public float rotateSpeed = 10f;
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+}
