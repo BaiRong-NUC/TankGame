71dd4bb77e0559d62da30fdd8b68492b39661b58
BaiRong-NUC <gby@edu.nuc.email>
2025-10-15 23:25:55 +0800

    完成了单例面板基类的构建

---
+++ Assets/Scripts/Game/BeginScence/Panel/Base/PanelBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 游戏各种面板的基类,单例基类
+// T 一定是一个类
+public class PanelBase<T> : MonoBehaviour where T : class
+{
+    private static T _instance;
+    public static T instance => _instance;
+
+    protected void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {
+            Destroy(this.gameObject); // 销毁重复的实例
+        }
+    }
+
+    // 面板打开
+    public virtual void OnOpen()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // 面板关闭
+    public virtual void OnClose()
+    {
+        gameObject.SetActive(false);
+    }
+}
