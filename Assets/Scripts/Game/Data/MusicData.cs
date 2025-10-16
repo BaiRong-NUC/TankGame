aee3f5b5c5ac58a1c794cdc173ccfe325aa1bb53
BaiRong-NUC <gby@edu.nuc.email>
2025-10-16 15:28:08 +0800

    完成基本音乐数据的封装

---
+++ Assets/Scripts/Game/Data/MusicData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicData
+{
+    // 背景音乐是否开启
+    public bool isOpenBk;
+    // 音效是否开启
+    public bool isOpenEffect;
+    // 音乐音量
+    public float musicVolume;
+    // 音效音量
+    public float effectVolume;
+}
---
+++ Assets/Scripts/Game/Data/MusicManage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 音乐管理类
+public class MusicManage
+{
+    private static MusicManage _instance;
+    public static MusicManage instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new MusicManage();
+            }
+            return _instance;
+        }
+    }
+
+    private MusicData musicData;
+
+    private MusicManage()
+    {
+        musicData = PlayerPrefsManage.instance.LoadData(typeof(MusicData), "MusicData") as MusicData;
+    }
+}
