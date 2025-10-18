65a02c2e6e991f739bbe3d840232f46ff3a87ccf
BaiRong-NUC <gby@edu.nuc.email>
2025-10-18 20:23:55 +0800

    完成游戏界面分数增加, 血量更新功能, 以及记录游戏时间的功能.

---
+++ Assets/Scripts/Game/Panel/GameScene/GamePanel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePanel : PanelBase<GamePanel>
+{
+    // 关联public控件对象
+    public GUILabel starLabel;
+    public GUILabel timeLabel;
+
+    public GUIButton quitBtn;
+    public GUIButton settingBtn;
+
+    public GUITexture healthyTexture;
+
+    public float maxHpWidth = 573f;
+
+    // 记录游玩时间
+    [HideInInspector]
+    public float gameTime = 0f;
+
+    // 记录分数
+    [HideInInspector]
+    public int star = 0;
+
+    void Start()
+    {
+        this.quitBtn.clickEvent += () =>
+        {
+            // 返回主菜单,弹出确定框
+        };
+
+        this.settingBtn.clickEvent += () =>
+        {
+        };
+    }
+
+    // 更新分数
+    public void AddStar(int star)
+    {
+        this.star += star;
+        this.starLabel.guiContent.text = this.star.ToString();
+    }
+
+    // 更新血量
+    public void UpdateHp(int maxHp, int currentHp)
+    {
+        this.healthyTexture.guiPos.width = ((float)currentHp / maxHp) * this.maxHpWidth;
+    }
+
+    void Update()
+    {
+        this.gameTime += Time.deltaTime;
+
+        // 将结果转为整数（向下取整，gameTime 为正数时 (int) 相当于 Floor）
+        int hours = (int)(this.gameTime / 3600f);
+        int minutes = (int)((this.gameTime % 3600f) / 60f);
+        int seconds = (int)(this.gameTime % 60f);
+
+        // 更新时间显示
+        this.timeLabel.guiContent.text = "";
+        if (hours > 0)
+        {
+            this.timeLabel.guiContent.text += hours + "时:";
+        }
+        if (minutes > 0 || hours > 0)
+        {
+            this.timeLabel.guiContent.text += minutes + "分:";
+        }
+        this.timeLabel.guiContent.text += seconds + "秒";
+    }
+}
