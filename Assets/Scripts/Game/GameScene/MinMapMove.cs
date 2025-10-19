d454624cf8778fac5654d78f1d6f758fe0f5ffcb
BaiRong-NUC <gby@edu.nuc.email>
2025-10-19 14:36:26 +0800

    完成小地图制作

---
+++ Assets/Scripts/Game/GameScene/MinMapMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinMapMove : MonoBehaviour
+{
+    //摄像机跟着目标动
+
+    public Transform target;
+    private Vector3 newPos;
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            newPos = target.position;
+            newPos.y = this.transform.position.y; // 保持摄像机的高度不变
+            this.transform.position = newPos;
+        }
+    }
+}
