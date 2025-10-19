6b02da20cb7793cfe1d3aeee80e80ad979d18c76
BaiRong-NUC <gby@edu.nuc.email>
2025-10-19 17:52:25 +0800

    添加了自动销毁特效脚本

---
+++ Assets/Scripts/Game/GameScene/AutoDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    public float time = 2;
+    // 自动移除爆炸特效节点
+    void Start()
+    {
+        Destroy(this.gameObject, time);
+    }
+}
