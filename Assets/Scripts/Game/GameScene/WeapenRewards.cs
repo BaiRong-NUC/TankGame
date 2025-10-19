c15325bd01c65f9525e7cc7c11a1403362582e6f
BaiRong-NUC <gby@edu.nuc.email>
2025-10-19 19:04:29 +0800

    添加了获取奖励的特效

--- Assets/Scripts/Game/GameScene/WeapenRewards.cs
+++ Assets/Scripts/Game/GameScene/WeapenRewards.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WeapenRewards : MonoBehaviour
 {
+    public GameObject rewardEffect;
     public GameObject[] weaponPrefabs;
     private void OnTriggerEnter(Collider other)
     {
@@ -17,7 +18,19 @@ public class WeapenRewards : MonoBehaviour
 
                 // 设置武器给玩家坦克
                 playerTank.SetWeapon(weaponObj);
+                if (this.rewardEffect != null)
+                {
+                    // 播放奖励特效
+                    GameObject effect = GameObject.Instantiate(rewardEffect, this.transform.position, this.transform.rotation);
 
+                    AudioSource audioSource = effect.GetComponent<AudioSource>();
+                    if (audioSource != null)
+                    {
+                        audioSource.mute = !GameDataManage.instance.musicData.isOpenEffect;
+                        audioSource.volume = GameDataManage.instance.musicData.effectVolume / 100f;
+                        audioSource.Play();
+                    }
+                }
                 // 销毁奖励物体
                 Destroy(this.gameObject);
             }
